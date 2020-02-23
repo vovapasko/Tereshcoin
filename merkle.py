d2df7ba6415ccec558f1bcc052fdb6fd00f3e002
vovapasko <vovapasko@gmail.com>
2020-02-23 17:10:14 +0200

    Started changing getTransactionProof

--- merkle.py
+++ merkle.py
@@ -10,101 +10,6 @@ def get_hash(string):
     return hashlib.sha256(hashlib.sha256(string.encode("utf-8")).hexdigest().encode('utf-8')).hexdigest()
 
 
-class MerkleTree:
-    def __init__(self, transactions):
-        self.transactions = sorted(transactions, key=lambda trx: trx.when)
-        self.nodes = []
-        self.createNodes()
-        self.root = self.getRoot()
-
-    def createNodes(self):
-        if len(self.transactions) == 1:
-            self.nodes.append(MerkleNode(self.transactions[0].__hash__(), self.transactions[0].__hash__()))
-        else:
-            for i in range(0, len(self.transactions), 2):
-                if i % 2 == 0 and i == len(self.transactions) - 1:
-                    self.nodes.append(MerkleNode(self.transactions[i].__hash__(), self.transactions[i].__hash__()))
-                else:
-                    self.nodes.append(MerkleNode(self.transactions[i].__hash__(), self.transactions[i + 1].__hash__()))
-
-    def getRootHash(self):
-        return self.root.my_hash
-
-    def getRootElement(self, nodes, roots):
-        if len(roots) == 1 and len(nodes) == 0:
-            return roots[0]
-        if len(nodes) == 0:
-            return self.getRootElement(roots, [])
-        if len(nodes) == 1:
-            roots.append(MerkleNode(nodes[0], nodes[0]))
-            return self.getRootElement(roots, [])
-        else:
-            roots.append(MerkleNode(nodes[0], nodes[1]))
-            new_nodes = nodes[2:]
-            return self.getRootElement(new_nodes, roots)
-
-    def getRoot(self):
-        root = self.getRootElement(self.nodes, [])
-        return root
-
-    def printTree(self, root):
-        new_root = root
-        left = new_root.getLeftChild()
-        right = new_root.getRightChild()
-        print(f"Root - {new_root}. It's hash - {new_root.__hash__()}")
-        print(f"Right - {right}. It's hash - {right.__hash__()}")
-        print(f"Left - {left}. It's hash - {left.__hash__()}")
-        print("---------------")
-        if isinstance(left, str) and isinstance(right, str):
-            return
-        self.printTree(left)
-        self.printTree(right)
-
-    def prove(self, root, searched_hash, proof):
-        if searched_hash == self.root.my_hash:
-            return proof
-        left_child = root.getLeftChild()
-        right_child = root.getRightChild()
-        if isinstance(left_child, str) and isinstance(right_child, str):  # means that it is the leaf of Merkle Tree
-            # todo try to replace the repeating code
-            if left_child == searched_hash:
-                new_searched_node = MerkleNode(searched_hash, right_child)
-                proof.append(right_child)
-                return self.prove(self.root, new_searched_node.my_hash, proof)
-            if right_child == searched_hash:
-                new_searched_node = MerkleNode(left_child, searched_hash)
-                proof.append(left_child)
-                return self.prove(self.root, new_searched_node.my_hash, proof)
-            return
-        if left_child.my_hash == searched_hash:
-            new_searched_node = MerkleNode(searched_hash, right_child.my_hash)
-            proof.append(right_child.my_hash)
-            return self.prove(self.root, new_searched_node.my_hash, proof)
-        if right_child.my_hash == searched_hash:
-            new_searched_node = MerkleNode(left_child.my_hash, searched_hash)
-            proof.append(left_child.my_hash)
-            return self.prove(self.root, new_searched_node.my_hash, proof)
-        self.prove(left_child, searched_hash, proof)
-        self.prove(right_child, searched_hash, proof)
-        return proof
-
-    def proveTransaction(self, trx_hash):
-        """
-        :param trx_hash: hash of transaction, presence of which should be proved
-        :return: dataset of needed transactions for reproducing Merkle tree and the hash of block if this transaction
-        belongs to tree and exists in chain. Otherwise returns empty list.
-        """
-        proof = []
-        proof = self.prove(self.root, trx_hash, proof)
-        return proof
-
-    def getNodeWithTransaction(self, trx_hash):
-        for node in self.nodes:
-            if node.getLeftChild() == trx_hash or node.getRightChild() == trx_hash:
-                return node
-        return False
-
-
 class MerkleNode:
     def __init__(self, leftChild, rightChild):
         self.leftChildHash = leftChild
@@ -152,6 +57,101 @@ class TransactionEncoder(JSONEncoder):
         if isinstance(o, Transaction):
             return o.__dict__
         return json.JSONEncoder.default(self, o)
+
+
+class MerkleTree:
+    def __init__(self, transactions):
+        self.transactions = sorted(transactions, key=lambda trx: trx.when)
+        self.nodes = []
+        self.createNodes()
+        self.root = self.getRoot()
+
+    def createNodes(self):
+        if len(self.transactions) == 1:
+            self.nodes.append(MerkleNode(self.transactions[0].__hash__(), self.transactions[0].__hash__()))
+        else:
+            for i in range(0, len(self.transactions), 2):
+                if i % 2 == 0 and i == len(self.transactions) - 1:
+                    self.nodes.append(MerkleNode(self.transactions[i].__hash__(), self.transactions[i].__hash__()))
+                else:
+                    self.nodes.append(MerkleNode(self.transactions[i].__hash__(), self.transactions[i + 1].__hash__()))
+
+    def getRootHash(self):
+        return self.root.my_hash
+
+    def getRootElement(self, nodes, roots):
+        if len(roots) == 1 and len(nodes) == 0:
+            return roots[0]
+        if len(nodes) == 0:
+            return self.getRootElement(roots, [])
+        if len(nodes) == 1:
+            roots.append(MerkleNode(nodes[0], nodes[0]))
+            return self.getRootElement(roots, [])
+        else:
+            roots.append(MerkleNode(nodes[0], nodes[1]))
+            new_nodes = nodes[2:]
+            return self.getRootElement(new_nodes, roots)
+
+    def getRoot(self):
+        root = self.getRootElement(self.nodes, [])
+        return root
+
+    def printTree(self, root: MerkleNode):
+        new_root = root
+        left = new_root.getLeftChild()
+        right = new_root.getRightChild()
+        print(f"Root - {new_root}. It's hash - {new_root.__hash__()}")
+        print(f"Right - {right}. It's hash - {right.__hash__()}")
+        print(f"Left - {left}. It's hash - {left.__hash__()}")
+        print("---------------")
+        if isinstance(left, str) and isinstance(right, str):
+            return
+        self.printTree(left)
+        self.printTree(right)
+
+    def prove(self, root, searched_hash, proof):
+        if searched_hash == self.root.my_hash:
+            return proof
+        left_child = root.getLeftChild()
+        right_child = root.getRightChild()
+        if isinstance(left_child, str) and isinstance(right_child, str):  # means that it is the leaf of Merkle Tree
+            # todo try to replace the repeating code
+            if left_child == searched_hash:
+                new_searched_node = MerkleNode(searched_hash, right_child)
+                proof.append(right_child)   # remaster the data structure here and add the flag of the side of leaf
+                return self.prove(self.root, new_searched_node.my_hash, proof)
+            if right_child == searched_hash:
+                new_searched_node = MerkleNode(left_child, searched_hash)
+                proof.append(left_child)
+                return self.prove(self.root, new_searched_node.my_hash, proof)
+            return  # means that leaf we are looking for is not in this subtree
+        if left_child.my_hash == searched_hash:
+            new_searched_node = MerkleNode(searched_hash, right_child.my_hash)
+            proof.append(right_child.my_hash)
+            return self.prove(self.root, new_searched_node.my_hash, proof)
+        if right_child.my_hash == searched_hash:
+            new_searched_node = MerkleNode(left_child.my_hash, searched_hash)
+            proof.append(left_child.my_hash)
+            return self.prove(self.root, new_searched_node.my_hash, proof)
+        self.prove(left_child, searched_hash, proof)
+        self.prove(right_child, searched_hash, proof)
+        return proof
+
+    def getTransactionProof(self, trx_hash):
+        """
+        :param trx_hash: hash of transaction, presence of which should be proved
+        :return: dataset of needed transactions for reproducing Merkle tree and the hash of block if this transaction
+        belongs to tree and exists in chain. Otherwise returns empty list.
+        """
+        proof = self.prove(self.root, trx_hash, proof)
+        return proof
+
+    def getNodeWithTransaction(self, trx_hash):
+        for node in self.nodes:
+            if node.getLeftChild() == trx_hash or node.getRightChild() == trx_hash:
+                return node
+        return False
+
 
 
 # str2 = '2f6806993fe7e7fe6d6048007e2d7d88b9ee21aad1bab6ee874886dc6a84c8d6' + '03bfcf8a24f2854983ea54ce45e48756a6d5f276bff0354b18109aa4b026b3eb'
--- merkle_proof.py
+++ merkle_proof.py
@@ -12,15 +12,14 @@ trx1 = Transaction('A', 'B', 1571866523.0888188, 150)
 trx2 = Transaction('C', 'D', 1571866523.0888188, 150)
 trx3 = Transaction('E', 'F', 1571866523.0888188, 150)
 trx4 = Transaction('G', 'H', 1571866523.0888188, 150)
-trx_lst = [trx1, trx2, trx3]
+trx_lst = [trx1, trx2, trx3, trx4]
 
 
 merkle_t = MerkleTree(trx_lst)
 print(merkle_t.getRootHash())
-
 trx_to_proof = Transaction('E', 'F', 1571866523.0888188, 150)
 trx_hash = trx_to_proof.__hash__()
-proof = merkle_t.proveTransaction(trx_hash)
+proof = merkle_t.getTransactionProof(trx_hash)
 
 address = get_hash("Vova")
 nodeV = Node(address)
